b92404185024ba28d835fcd13aa3aafcba48dc85
Mr.Kumar <dilipkumer092@gmail.com>
2023-02-28 20:06:57 +0530

    leedcode 3rd

--- Product_of_Array_Except_Self.py
+++ Product_of_Array_Except_Self.py
@@ -9,21 +9,25 @@ def Product_of_Array_Except(nums):
     right_product = []
     j=0
     for i in range(len(nums)-1,-1,-1):
-        if i == 0:
+        if i == len(nums)-1:
             right_product.append(nums[i])
         else:
-            right_product.append(right_product[i-1]*nums[i])
+            right_product.append(right_product[j-1]*nums[i])
         j+=1
         right_product = right_product[::-1]
 
     result = []
-    for i in range(0,len(num)):
-        if i== 0
+    for i in range(0,len(nums)):
+        if i== 0:
+            result.append(1*right_product[i+1])
+        elif i == len(nums)-1:
+            result.append(left_product[i-1]*1)
 
-    
+        else:
+            result.append(left_product[i-1]*right_product[i+1])
 
 
-    return nums
+    return result
     
 
 
